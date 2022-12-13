a3b745390c7cebe6e0b0f3e6255a620ccbce499e
DavitaR <92464615+DavitaR@users.noreply.github.com>
2022-12-13 02:56:02 -0500

    4th iq

---
+++ Exercises/getTotalPrice.js
@@ -0,0 +1,57 @@
+/*
+Create a function that takes an array of objects (groceries) which calculates the total price and returns it as a number. A grocery object has a product, a quantity and a price, for example:
+
+{
+  "product": "Milk",
+  "quantity": 1,
+  "price": 1.50
+}
+Examples
+// 1 bottle of milk:
+getTotalPrice([
+  { product: "Milk", quantity: 1, price: 1.50 }
+]) ➞ 1.5
+
+// 1 bottle of milk & 1 box of cereals:
+getTotalPrice([
+  { product: "Milk", quantity: 1, price: 1.50 },
+  { product: "Cereals", quantity: 1, price: 2.50 }
+]) ➞ 4
+
+// 3 bottles of milk:
+getTotalPrice([
+  { product: "Milk", quantity: 3, price: 1.50 }
+]) ➞ 4.5
+
+// Several groceries:
+getTotalPrice([
+  { product: "Milk", quantity: 1, price: 1.50 },
+  { product: "Eggs", quantity: 12, price: 0.10 },
+  { product: "Bread", quantity: 2, price: 1.60 },
+  { product: "Cheese", quantity: 1, price: 4.50 }
+]) ➞ 10.4
+
+// Some cheap candy:
+getTotalPrice([
+  { product: "Chocolate", quantity: 1, price: 0.10 },
+  { product: "Lollipop", quantity: 1, price: 0.20 }
+]) ➞ 0.3
+Notes
+There might be a floating point precision problem in here...
+*/
+
+//put in array of objects, each with
+function getTotalPrice(array) {
+  let total = 0;
+  let i;
+  for (i = 0; i < array.length; i++) {
+    let itemMoneyOwed = array[i].price * array[i].quantity;
+    total += itemMoneyOwed;
+  }
+
+  let finalTotal = +parseFloat(total).toFixed(1); //converts string back to float
+
+  return finalTotal;
+}
+
+exports.solution = getTotalPrice;
